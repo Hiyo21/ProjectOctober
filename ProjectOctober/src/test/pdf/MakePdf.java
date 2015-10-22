b25e983144fce545bc11cb9eb84e4c38015bb640
jihee kim <jihee881004@nate.com>
2015-10-22 20:54:59 +0900

    진짜 짜증

--- ProjectOctober/src/test/pdf/MakePdf.java
+++ ProjectOctober/src/test/pdf/MakePdf.java
@@ -14,31 +14,6 @@ public class MakePdf {
 	}
 	public static boolean processPdf(){
 		String strHtml = targethtml;
-        String strCmd = "G:\\program\\wkhtmltopdf\\bin\\wkhtmltopdf.exe";
-        strCmd += " " + strHtml;
-        strCmd += " " + savefile;
-        
-        Pdf t = new Pdf();
-        System.out.println(  t.runCmd(strCmd)  );
-        return true;
-	}
-}
-package test.pdf;
-
-public class MakePdf {
-	static String savefile;
-	static String targethtml;
-	public MakePdf(String savefile,String targethtml){
-		this.savefile = savefile;
-		this.targethtml= targethtml;
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		processPdf();
-	}
-	public static boolean processPdf(){
-		String strHtml = targethtml;
         String strCmd = "D:\\program\\wkhtmltopdf\\bin\\wkhtmltopdf.exe --no-stop-slow-scripts --javascript-delay 3000 --orientation Landscape";
         strCmd += " " + strHtml;
         strCmd += " " + savefile;
