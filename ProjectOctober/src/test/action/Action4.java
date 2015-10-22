753b16922113aec2b929e4e70de5cb946cab3169
Jihee881004 <jihee881004@nate.com>
2015-10-22 14:44:37 +0900

    통계 4 나오게 만듦

--- ProjectOctober/src/test/action/Action4.java
+++ ProjectOctober/src/test/action/Action4.java
@@ -10,6 +10,7 @@ import com.opensymphony.xwork2.ActionSupport;
 
 import test.dao.DAO;
 import test.vo.Highchart4;
+import test.vo.Highchart4Add;
 
 public class Action4 extends ActionSupport implements RequestAware{
 	private Map<String, Object> request;
@@ -24,40 +25,59 @@ public class Action4 extends ActionSupport implements RequestAware{
 		List<Highchart4> list = dao.highchart4DAO();
 		System.out.println(list.toString()+"여기");
 		
-		List<Highchart4> listJubu = new ArrayList<Highchart4>();
-		List<Highchart4> listFree = new ArrayList<Highchart4>();
-		List<Highchart4> listOther = new ArrayList<Highchart4>();
-		List<Highchart4> listCompany = new ArrayList<Highchart4>();
-		List<Highchart4> listStudent = new ArrayList<Highchart4>();
-		List<Highchart4> listExpert = new ArrayList<Highchart4>();
+		Highchart4Add vo1 = new Highchart4Add();
+		Highchart4Add vo2 = new Highchart4Add();
+		Highchart4Add vo3 = new Highchart4Add();
+		Highchart4Add vo4 = new Highchart4Add();
+		Highchart4Add vo5 = new Highchart4Add();
+		Highchart4Add vo6 = new Highchart4Add();
+		
+		
+		
+		List<Highchart4Add> listJubu1 = new ArrayList<Highchart4Add>();
+		List<Highchart4Add> listFree1 = new ArrayList<Highchart4Add>();
+		List<Highchart4Add> listOther1 = new ArrayList<Highchart4Add>();
+		List<Highchart4Add> listCompany1 = new ArrayList<Highchart4Add>();
+		List<Highchart4Add> listStudent1 = new ArrayList<Highchart4Add>();
+		List<Highchart4Add> listExpert1 = new ArrayList<Highchart4Add>();
 		
 
 		for(Highchart4 temp : list){
 			if(temp.getCstJob().equals("주부")){
-				listJubu.add(temp);
+				vo1.setJubu1(temp.getMoneyflow());
 			}else if(temp.getCstJob().equals("프리랜서")){
-				listFree.add(temp);
+				vo2.setFree1(temp.getMoneyflow());
 			}else if(temp.getCstJob().equals("기타")){
-				listOther.add(temp);
+				vo3.setOther1(temp.getMoneyflow());
 			}else if(temp.getCstJob().equals("회사원")){
-				listCompany.add(temp);
+				vo4.setCompany1(temp.getMoneyflow());
 			}else if(temp.getCstJob().equals("학생")){
-				listStudent.add(temp);
+				vo5.setStudent1(temp.getMoneyflow());
 			}else if(temp.getCstJob().equals("전문직")){
-				listExpert.add(temp);
+				vo6.setExpert1(temp.getMoneyflow());
 			}
 		}
 		
-		System.out.println(listJubu.toString());
-			
-		request.put("listJubu",listJubu);
-		request.put("listFree",listFree);
-		request.put("listOther",listOther);
-		request.put("listCompany",listCompany);
-		request.put("listStudent",listStudent);
-		request.put("listExpert",listExpert);
+		listJubu1.add(vo1);
+		listFree1.add(vo2);
+		listOther1.add(vo3);
+		listCompany1.add(vo4);
+		listStudent1.add(vo5);
+		listExpert1.add(vo6);
 		
-				
+
+			
+		request.put("listJubu1",listJubu1);
+		request.put("listFree1",listFree1);
+		request.put("listOther1",listOther1);
+		request.put("listCompany1",listCompany1);
+		request.put("listStudent1",listStudent1);
+		request.put("listExpert1",listExpert1);
+		
+		System.err.println(request.get("listJubu1"));	
+		System.err.println(request.get("listFree1"));
+		System.err.println(request.get("listOther1"));
+		System.err.println(request.get("listCompany1"));
 		return SUCCESS;
 	}
 
